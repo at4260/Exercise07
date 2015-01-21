43b7bc59e6829d83b92081db7b0c7e4b0daafa40
Hackbright Student <info@hackbrightacademy.com>
2015-01-21 12:00:22 -0800

    ex7 initial run complete

--- wordcount.py
+++ wordcount.py
@@ -2,19 +2,27 @@ from sys import argv
 
 script, filename = argv
 
-f = open(filename)
-new_dict = {}
-# create a loop that will split by whitespace
-for line in f:
-    line = line.rstrip()
-    words = line.split(' ')
-# check if the word is in the current dictionary; if yes, add 1 to the value; if no, add the word as a key and 1 as the value
-    for word in words:
-        if word in new_dict:
-            new_dict[word] = new_dict[word] + 1
-        else:
-            new_dict[word] = 1
 
+def create_dict():
+
+    f = open(filename)
+    new_dict = {}
+    # create a loop that will split by whitespace
+    for line in f:
+        line = line.rstrip()
+        words = line.split(' ')
+    # check if the word is in the current dictionary; if yes, add 1 to the value; if no, add the word as a key and 1 as the value
+        for word in words:
+            if word in new_dict:
+                new_dict[word] = new_dict[word] + 1
+            else:
+                new_dict[word] = 1
+
+    return new_dict
 # print word count
+def print_dict(dictionary):
+    for k, v in dictionary.items():
+        print k, v
 
-
+new_dict = create_dict()
+print_dict(new_dict)
